c7b8aa640c526abb40f0eaeae992c2b42b26fe0f
GasbroNinja <gasbroider@live.it>
2023-03-29 09:31:28 +0200

    da modificare la img in locale "public"

--- src/App.jsx
+++ src/App.jsx
@@ -9,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <Image img="https://konachan.net/jpeg/c994a53414e877d2eb2a89387b1f47c3/Konachan.com%20-%20354837%20barefoot%20blue_eyes%20blue_hair%20blush%20butterfly_hair_ornament%20dress%20flowers.jpg" alt="anime" />
+        <Image img="..." alt="anime" />
         <Button testo="Mollami JS" />
 
       </header>
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -3,7 +3,7 @@ import React from "react"
 class LelloImg extends React.Component {
   render() {
     console.log(LelloImg.props)
-    return <img style={{boxShadow:"2px 2px 25px 1px white", border:"2px solid #454c5a"}} src={this.props.img} alt={this.props.alt} />
+    return <img style={{boxShadow:"2px 2px 25px 1px white", border:"2px solid #454c5a"}} src={this.props.src} alt={this.props.alt} />
     
   }
 }
